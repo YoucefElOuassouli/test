8a89cee0f55a7f03d044b693a15a9f1ce6244ac1
elouassouli <65324726+elouassouli@users.noreply.github.com>
2021-02-24 18:06:27 +0100

    csharp

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EcommerceSite.Startup))]
+namespace EcommerceSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
